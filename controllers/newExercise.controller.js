a172067ce8e5c7bb2b7d9078797147fd10196534
Camille Hagenbourger <camille.hagenbourger@gmail.com>
2018-11-23 10:27:39 +0100

    fix date management

--- controllers/newExercise.controller.js
+++ controllers/newExercise.controller.js
@@ -2,14 +2,15 @@ const User = require('../models/user.model');
 const Exercise = require('../models/exercise.model');
 const userFunctions = require('./common/userFunctions');
 
-exports.add_new_exercise = async function(req, res) {
+exports.add_new_exercise = async function (req, res) {
     //ensure that user id is registered
     let user = await userFunctions.user_exists_by_id(req);
     if (!user) {
         res.send("Error, incorrect id.");
         return
     }
-    create_and_display_new_exercise(req, res, user);
+
+    await create_and_display_new_exercise(req, res, user);
 }
 
 const create_and_display_new_exercise = async function (req, res, user) {
@@ -18,17 +19,37 @@ const create_and_display_new_exercise = async function (req, res, user) {
             user_id: user._id,
             description: req.body.description,
             duration: req.body.duration
-        })
+        });
+        if (isDateFormatValid(req.body.date)) {
+            exercise.date = req.body.date;
+        }
+        else {
+            res.send("Invalid date format : please use yyyy-mm-dd");
+            return
+        }
+
         exercise.save(function (err) {
             if (err) return handleError(err)
-            res.json({ 
-                User: {username: user.username, _id: user._id}, 
-                Exercise:{description: exercise.description, duration: exercise.duration, date: exercise.date} 
+            res.json({
+                User: { username: user.username, _id: user._id },
+                Exercise: { description: exercise.description, duration: exercise.duration, date: exercise.date }
             });
             console.log("New exercise added to the database");
         })
-    } catch(e) {
-        console.log("error while registering new exercise");
+    } catch (e) {
+        console.log("error while saving exercise to db");
         return
     }
+}
+
+function isDateFormatValid(date) {
+    let regexp = /^(\d{4})-(\d{2})-(\d{2})$/;
+    let result = date.match(regexp);
+    if (result === null) { return false };
+    if (result[1] > 1900 && result[1] < 2100 && result[2] > 0 && result[2] < 13 && result[3] > 0 && result[3] < 32) {
+        console.log("valid");
+        return true;
+    }
+    console.log("invalid");
+    return false;
 }
